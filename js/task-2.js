e8448a4fd4e9976f02e34dabd294c41394127f83
albert <adalbert.iceberg@gmail.com>
2020-11-15 19:49:20 +0300

    JS-2

--- js/task-2.js
+++ js/task-2.js
@@ -5,7 +5,7 @@ function print(str) {
 window.onload = function () {
 
     for (var i = 1; i <= 100; i++) {
-        if (i % 5 === 0 & i % 3 === 0) {
+        if (i % 5 === 0 && i % 3 === 0) {
             print('FizzBuzz');
         } else {
             if (i % 3 === 0) {
@@ -19,5 +19,25 @@ window.onload = function () {
                 }
             }
         }
+    }
+}
+
+window.onload = function () {
+    for (let i = 1; i <= 100; i++) {
+        let str = '';
+
+        if (i % 3 === 0) {
+            str += 'Fizz';
+        }
+
+        if (i % 5 === 0) {
+            str += 'Buzz';
+        }
+
+        if ( str === '') {
+            str = i;
+        }
+
+        print(str);
     }
 }
