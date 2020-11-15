8d49c8b9b437006d3cbd1624caaee97abd46ba48
albert <adalbert.iceberg@gmail.com>
2020-11-15 19:30:38 +0300

    JS-2

---
+++ js/task-2.js
@@ -0,0 +1,23 @@
+function print(str) {
+    document.getElementById("content").innerHTML += str + '<br>';
+}
+
+window.onload = function () {
+
+    for (var i = 1; i <= 100; i++) {
+        if (i % 5 === 0 & i % 3 === 0) {
+            print('FizzBuzz');
+        } else {
+            if (i % 3 === 0) {
+                // print(regina)
+                print('Fizz');
+            } else {
+                if (i % 5 === 0) {
+                    print('Buzz');
+                } else {
+                    print(i);
+                }
+            }
+        }
+    }
+}
